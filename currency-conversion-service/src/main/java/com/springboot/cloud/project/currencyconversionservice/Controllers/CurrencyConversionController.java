fe0062482f3b52e71ac919bfc9995c926f932f2c
himanshu14263 <himanshu14263@gmail.com>
2020-03-15 14:57:21 +0530

    getting data from currency-exchange-service using RestTemplate

--- currency-conversion-service/src/main/java/com/springboot/cloud/project/currencyconversionservice/Controllers/CurrencyConversionController.java
+++ currency-conversion-service/src/main/java/com/springboot/cloud/project/currencyconversionservice/Controllers/CurrencyConversionController.java
@@ -1,11 +1,16 @@
 package com.springboot.cloud.project.currencyconversionservice.Controllers;
 
 import com.springboot.cloud.project.currencyconversionservice.Beans.CurrencyConversionBean;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import java.math.BigDecimal;
+import java.security.URIParameter;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class CurrencyConversionController {
@@ -14,8 +19,22 @@ public class CurrencyConversionController {
                                                   @PathVariable String to,
                                                   @PathVariable BigDecimal quantity){
 
-        CurrencyConversionBean currencyConversionBean = new CurrencyConversionBean(1L,from,to,new BigDecimal(65),
-                quantity ,new BigDecimal(6500),1010);
+        Map<String, String> uriVariables = new HashMap<>();
+        uriVariables.put("from",from);
+        uriVariables.put("from",to);
+
+        //calling currency-exchange service
+        ResponseEntity<CurrencyConversionBean> responseEntity =  new RestTemplate().getForEntity("http://localhost:8000/currency-exchange/from/{from}/to/{to}",CurrencyConversionBean.class, uriVariables);
+
+        //reading extracting data response Entity
+        CurrencyConversionBean response = responseEntity.getBody();
+
+        //reading data which will be used for calculation
+        BigDecimal conversionMultiple = response.getConversionMultiple();
+        int port = response.getPort();
+
+        CurrencyConversionBean currencyConversionBean = new CurrencyConversionBean(1L,from,to, conversionMultiple,
+                quantity ,conversionMultiple.multiply(quantity),port);
 
         return currencyConversionBean;
     }
